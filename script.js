30f92031bca8f352c84e12ac98563b3a185a0a91
JackBrasesco <jacbras@nuevaschool.org>
2016-11-29 08:45:10 -0800

    Classadoo autocommit

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 var button = $("#btn");
 
-
+var btnpress = 0;
 
 button.click(function() {
   dealWithIt()
 })
 
 
-
+function switchT
 function dealWithIt() {
     $("#glasses").animate({top: "283px" }, 1500)
 }
